aac1f3d0d78296511e2d2ee784f74482e0fb1f5a
TheSalle <salezivkovic@live.com>
2024-08-14 17:49:11 +0200

    kreirani resursi

---
+++ app/app/Http/Resources/DocumentResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DocumentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'user' => new UserResource($this->user),
+            'file_path' => $this->file_path,
+            'comments' => CommentResource::collection($this->comments),
+            'tags' => TagResource::collection($this->tags),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
